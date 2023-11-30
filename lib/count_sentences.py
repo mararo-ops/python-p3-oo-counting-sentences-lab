758599196fd5c98c33f05c1b4e7e13def74ad7c0
mararo-ops <danielmararo1@gmail.com>
2023-11-30 21:55:02 +0300

    cmpleted assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,26 @@
-#!/usr/bin/env python3
-
 class MyString:
-  pass
+    def __init__(self, value=""):
+        if not isinstance(value, str):
+            raise ValueError("The value must be a string.")
+        self.value = value
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        # Replace common punctuation with periods and split the string
+        sentences = self.value.replace('?', '.').replace('!', '.').split('.')
+        
+        # Remove empty strings from the list
+        sentences = [sentence.strip() for sentence in sentences if sentence.strip()]
+        
+        return len(sentences)
+
+
+
